a8319cc69ba2d2c0b9329cd547a372005c9cfd67
DiogoBruno06 <diogobruno06@outlook.com>
2023-07-14 10:18:48 -0300

    Correcao questao 10

--- diogo/vs12-back/modulo-01-java/aula-01/task-01/task/src/Questao9.java
+++ diogo/vs12-back/modulo-01-java/aula-01/task-01/task/src/Questao9.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Questao9 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite a data, hora e minuto de entrada seguindo o pattern dd--mm-yyyy T hh:mm");
         LocalDateTime entrada = LocalDateTime.parse(teclado.next());
+        System.out.println("Digite a data, hora e minuto de saida seguindo o pattern dd--mm-yyyy T hh:mm");
         LocalDateTime saida = LocalDateTime.parse(teclado.next());
 
         Duration diferenca = Duration.between(entrada,saida);
